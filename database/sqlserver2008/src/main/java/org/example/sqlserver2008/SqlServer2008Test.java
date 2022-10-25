a94b282ae8c54a64ceb2bee09cecb608416729a5
zhengjinlong <1061917196@qq.com>
2022-10-25 09:50:13 +0800

    feat: oracleTest

--- database/sqlserver2008/src/main/java/org/example/sqlserver2008/SqlServer2008Test.java
+++ database/sqlserver2008/src/main/java/org/example/sqlserver2008/SqlServer2008Test.java
@@ -11,11 +11,13 @@ import java.sql.DriverManager;
  */
 public class SqlServer2008Test {
     public static void main(String[] args) throws Exception {
-        //192.168.75.128
+        String url = System.getProperty("url");
         Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-        String url = "jdbc:sqlserver://172.22.160.162:1433;DatabaseName=test";
-        String usrname = "sa";
-        String passWord = "zhjl951753";
+        if(url==null){
+            url = "jdbc:sqlserver://172.22.160.162:1433;DatabaseName=test";
+        }
+        String usrname = System.getProperty("username");
+        String passWord = System.getProperty("password");
         Connection con = DriverManager.getConnection(url,usrname,passWord);
         System.out.println("数据库连接成功。");
     }

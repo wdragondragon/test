318b53d23d69f31387bc9a2510e7827801cee461
zhengjinlong <1061917196@qq.com>
2023-02-15 17:06:37 +0800

    test: ora

--- database/mysql8/src/main/java/org/example/mysql8/App.java
+++ database/mysql8/src/main/java/org/example/mysql8/App.java
@@ -11,7 +11,7 @@ import java.sql.DriverManager;
 public class App {
     public static void main(String[] args) throws Exception {
         String url = System.getProperty("url");
-        Class.forName("com.mysql.cj.jdbc.Driver");
+        Class.forName("com.mysql.jdbc.Driver");
         if(url==null){
             url = "jdbc:mysql://10.15.6.45:3306/partybuild";
         }
